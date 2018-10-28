706f4d1a33f884dcbbff2b50d6975a827bb93cb3
Igor <igodevs@gmail.com>
2018-10-28 21:25:40 +0100

    add web config file1

--- index.js
+++ index.js
@@ -3,18 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
-
-
 app.use(cors());
 app.use(bodyParser.json());
 
-//home page
 app.get('/', (req, res) => {
-    //get all events
     res.send("Working")
 });
-
-
 
 app.listen(3000 || process.env.PORT, () => {
     console.log('app is running on port 3000');
